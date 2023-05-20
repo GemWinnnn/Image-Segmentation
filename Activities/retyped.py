e8454368e707a168c447ffdcfb39ea661e1f85f6
GemWinnnn <gemwin.canete@wvsu.edu.ph>
2023-05-20 17:19:30 +0800

    lol

--- Activities/retyped.py
+++ Activities/retyped.py
@@ -14,7 +14,7 @@ import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from keras.utils import to_categorical
 from keras.applications import inception_v3
-
+import seaborn as sns 
 
 
 class_names = ['HEADPHONE','HARDDRIVE','MOUSE','GLASSES']
@@ -179,208 +179,209 @@ for i, frame in enumerate(raw_frames_type_4):
     plt.axis('off')
     plt.show()
 
-# Load images from 'img_1' directory
-for image_path in glob('img_1/*.png'):
-    img = Image.open(image_path).convert('RGB')
-    img = img.resize((width, height))
-    img_array = np.array(img)
-    images_type_1.append(img_array)
-    # labels.append(class_index)
 
-# Load images from 'img_2' directory
-for image_path in glob('img_2/*.png*'):
-    img = Image.open(image_path).convert('RGB')
-    img = img.resize((width, height))
-    img_array = np.array(img)
-    images_type_2.append(img_array)
-    # labels.append(class_index)
-
-# Load images from 'img_3' directory
-for image_path in glob('img_3/*.png*'):
-    img = Image.open(image_path).convert('RGB')
-    img = img.resize((width, height))
-    img_array = np.array(img)
-    images_type_3.append(img_array)
-    # labels.append(class_index)
-
-# Load images from 'img_4' directory
-for image_path in glob('img_4/*.png*'):
-    img = Image.open(image_path).convert('RGB')
-    img = img.resize((width, height))
-    img_array = np.array(img)
-    images_type_4.append(img_array)
-    # labels.append(class_index)
-
-
-print('Shape of images_type_1:', images_type_1[0].shape)
-print('Shape of images_type_2:', images_type_2[0].shape)
-print('Shape of images_type_3:', images_type_3[0].shape)
-print('Shape of images_type_4:', images_type_4[0].shape)
+for image_path in glob('img_1/*.png*'):
+    image = preprocessing.image.load_img(image_path, target_size=(width, height))
+    x = preprocessing.image.img_to_array(image)
     
-plt.figure(figsize=(12,8))
+    images_type_1.append(x)
 
-# Generate visualization for images_type_1
+for image_path in glob('img_2/*.png*'):
+    image = preprocessing.image.load_img(image_path, target_size=(width, height))
+    x = preprocessing.image.img_to_array(image)
+    
+    images_type_2.append(x)
+    
+for image_path in glob('img_3/*.png*'):
+    image = preprocessing.image.load_img(image_path, target_size=(width, height))
+    x = preprocessing.image.img_to_array(image)
+    
+    images_type_3.append(x)
+
+for image_path in glob('img_4/*.png*'):
+    image = preprocessing.image.load_img(image_path, target_size=(width, height))
+    x = preprocessing.image.img_to_array(image)
+    
+    images_type_4.append(x)
+    
+plt.figure(figsize=(12, 8))
+
 for i, x in enumerate(images_type_1[:5]):
+    
     plt.subplot(1, 5, i+1)
-    image = Image.fromarray(np.uint8(x))
+    image = preprocessing.image.array_to_img(x)
     plt.imshow(image)
+    
     plt.axis('off')
     plt.title('{} image'.format(class_names[0]))
+
 plt.show()
 
-plt.figure(figsize=(12, 8))
-
-# Generate visualization for images_type_2
 for i, x in enumerate(images_type_2[:5]):
+    
     plt.subplot(1, 5, i+1)
-    image = Image.fromarray(np.uint8(x))
+    image = preprocessing.image.array_to_img(x)
     plt.imshow(image)
+    
     plt.axis('off')
     plt.title('{} image'.format(class_names[1]))
+
 plt.show()
 
-plt.figure(figsize=(12, 8))
-
-# Generate visualization for images_type_3
 for i, x in enumerate(images_type_3[:5]):
+        
     plt.subplot(1, 5, i+1)
-    image = Image.fromarray(np.uint8(x))
+    image = preprocessing.image.array_to_img(x)
     plt.imshow(image)
+    
     plt.axis('off')
     plt.title('{} image'.format(class_names[2]))
+
 plt.show()
 
-plt.figure(figsize=(12, 8))
-
-# Generate visualization for images_type_4
 for i, x in enumerate(images_type_4[:5]):
+    
     plt.subplot(1, 5, i+1)
-    image = Image.fromarray(np.uint8(x))
+    image = preprocessing.image.array_to_img(x)
     plt.imshow(image)
+    
     plt.axis('off')
     plt.title('{} image'.format(class_names[3]))
+    
 plt.show()
 
-
 # Prepare Image to Tensor
+
 X_type_1 = np.array(images_type_1)
 X_type_2 = np.array(images_type_2)
 X_type_3 = np.array(images_type_3)
 X_type_4 = np.array(images_type_4)
 
-
-#Check the image shape using .shape()
+# Check the shape using .shape() check the images count
 
 print (X_type_1.shape)
 print (X_type_2.shape)
 print (X_type_3.shape)
 print (X_type_4.shape)
 
+(13, 96, 96, 3)
+(23, 96, 96, 3)
+(14, 96, 96, 3)
+(22, 96, 96, 3)
 
- 
+X_type_2
+
 X = np.concatenate((X_type_1, X_type_2), axis=0)
 
-if len (X_type_3):
+if len(X_type_3):
     X = np.concatenate((X, X_type_3), axis=0)
-    
-if len (X_type_4):
+
+if len(X_type_4):
     X = np.concatenate((X, X_type_4), axis=0)
-
-#Scaling the data to 1-0
-
-X = X/225.0
-
-
-y_type_1 = [0 for item in enumerate (X_type_1)]
-y_type_2 = [1 for item in enumerate (X_type_2)]
-y_type_3 = [2 for item in enumerate (X_type_3)]
-y_type_4 = [3 for item in enumerate (X_type_4)]
-
-y = np.concatenate((y_type_1,y_type_2),axis=0)
-
-if len (y_type_3):
-    y = np.concatenate((y,y_type_3),axis=0)
     
-if len (y_type_4):
-    y = np.concatenate((y,y_type_4),axis=0)
-    
-y = to_categorical(y,num_classes=len(class_names))
+# Scaling the data to 1 - 0
 
-print(y.shape)
+X = X / 255.0
+
+X.shape
+
+(72, 96, 96, 3)
+
+from keras.utils import to_categorical
+
+y_type_1 = [0 for item in enumerate(X_type_1)]
+y_type_2 = [1 for item in enumerate(X_type_2)]
+y_type_3 = [2 for item in enumerate(X_type_3)]
+y_type_4 = [3 for item in enumerate(X_type_4)]
+
+y = np.concatenate((y_type_1, y_type_2), axis=0)
+
+if len(y_type_3):
+    y = np.concatenate((y, y_type_3), axis=0)
+
+if len(y_type_4):
+    y = np.concatenate((y, y_type_4), axis=0)
+    
+y = to_categorical(y, num_classes=len(class_names))
+
+y.shape
+
 (72, 4)
 
-#default Parameters
+
+# Situational - values, you may not adjust these
 
 conv_1 = 16
 conv_1_drop = 0.2
-
 conv_2 = 32
 conv_2_drop = 0.2
-
 dense_1_n = 1024
 dense_1_drop = 0.2
-
-dense_2_n =512
+dense_2_n = 512
 dense_2_drop = 0.2
 
+# Values you can adjust
 lr = 0.001
-epochs = 15
-batch_size =10
-color_channels =  3
+epochs = 5
+batch_size = 10
+color_channels = 3
 
-
-def build_model (conv_1_drop = conv_1_drop, conv_2_drop = conv_2_drop, 
-                 dense_1_drop = dense_1_drop, dense_1_n = dense_1_n,
-                 dense_2_n = dense_2_n, dense_2_drop = dense_2_drop,
-                 lr = lr):
+def build_model(conv_1_drop = conv_1_drop, conv_2_drop = conv_2_drop,
+                dense_1_n = dense_1_n, dense_1_drop = dense_1_drop,
+                dense_2_n = dense_2_n, dense_2_drop = dense_2_drop,
+                lr = lr):
     
     model = Sequential()
     
-    model.add(Convolution2D(conv_1,(3,3),input_shape = (width,height, color_channels),
-                            activation = 'relu'))
+    model.add(Convolution2D(conv_1, (3, 3),
+                            input_shape = (width, height, color_channels),
+                            activation='relu'))
     
-    model.add (MaxPooling2D(pool_size=(2,2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     
-    model.add (Dropout(conv_1_drop))
+    model.add(Dropout(conv_1_drop))
     
-
-    model.add(Convolution2D(conv_2,(3,3),activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add (Dropout(conv_2_drop))
+    # ---
     
+    model.add(Convolution2D(conv_2, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(conv_1_drop))
     
+    # ---
     model.add(Flatten())
     
-    model.add(Dense(dense_1_n , activation='relu'))
+    # ---
+    model.add(Dense(dense_1_n, activation='relu'))
     model.add(Dropout(dense_1_drop))
     
-    
-    model.add(Dense(dense_2_n , activation='relu'))
+    # ---
+    model.add(Dense(dense_2_n, activation='relu'))
     model.add(Dropout(dense_2_drop))
     
-    model.add (Dense(len(class_names),activation='softmax'))
+    # ---
+    model.add(Dense(len(class_names), activation='softmax'))
     
     model.compile(loss='categorical_crossentropy',
-                  optimizer = Adam(clipvalue=0.5),
+                  optimizer=Adam(clipvalue=0.5),
                   metrics=['accuracy'])
     
     return model
 
 # model parameter
+
 model = build_model()
 
 model.summary()
 
-history = model.fit(X,y, validation_split=0.10,epochs=epochs,batch_size=5)
+# Do not run yet
 
-print(history)
+history = model.fit(X, y, validation_split=0.10, epochs=10, batch_size=5)
 
-#model evaluation
+print(history) 
 
-scores = model.evaluate(X, y, verbose='verbose')
+# Model evaluation
+scores = model.evaluate(X, y, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
-
-# Accuracy: 100.00%
 
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
@@ -404,11 +405,12 @@ plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper right')
 plt.show()
 
-#Prediction
+# Prediction
+
 def plt_show(img):
     plt.imshow(img)
     plt.show()
-
+    
 headphone = 'img_1/10.png'
 mouse = 'img_2/10.png'
 harddrive = 'img_3/10.png'
@@ -416,31 +418,27 @@ glasses = 'img_4/10.png'
 
 imgs = [headphone, mouse, harddrive, glasses]
 
-#def predict_(img_path):
+# def predict_(img_path):
+
 classes = None
 predicted_classes = []
-true_labels = []
 
 for i in range(len(imgs)):
-    img = Image.open(imgs[i]).convert('RGB')
-    img = img.resize((width, height))
-    plt.imshow(img)
+    type_ = preprocessing.image.load_img(imgs[i], target_size=(width, height))
+    plt.imshow(type_)
     plt.show()
     
-    type_x = np.expand_dims(img, axis=0)
+    type_x = np.expand_dims(type_, axis=0)
     prediction = model.predict(type_x)
     index = np.argmax(prediction)
     print(class_names[index])
     classes = class_names[index]
     predicted_classes.append(class_names[index])
     
-    true_labels.append(class_names[i % len(class_names)])
+cm = confusion_matrix(class_names, predicted_classes)
+f = sns.heatmap(cm, xticklabels=class_names, yticklabels=predicted_classes, annot=True)
 
-cm = confusion_matrix(true_labels, predicted_classes)
-f = sns.heatmap(cm, xticklabels=class_names, yticklabels=class_names, annot=True)
-
-type_1 = Image.open('img_1/10.png').convert('RGB')
-type_1 = type_1.resize((width, height))
+type_1 = preprocessing.image.load_img('img_1/10.png', target_size=(width, height))
 
 plt.imshow(type_1)
 plt.show()
@@ -451,96 +449,73 @@ index = np.argmax(predictions)
 
 print(class_names[index])
 
-type_2 = Image.open('img_2/10.png').convert('RGB')
-type_2 = type_2.resize((width, height))
+type_2 = preprocessing.image.load_img('img_2/10.png', target_size=(width, height))
 
 plt.imshow(type_2)
 plt.show()
 
 type_2_x = np.expand_dims(type_2, axis=0)
 predictions = model.predict(type_2_x)
-index = np.argmax(predictions)
 
+index = np.argmax(predictions)
 print(class_names[index])
 
-type_3 = Image.open('img_3/10.png').convert('RGB')
-type_3 = type_3.resize((width, height))
+# Live Predictions using camera
 
-plt.imshow(type_3)
-plt.show()
-
-type_3_x = np.expand_dims(type_3, axis=0)
-predictions = model.predict(type_3_x)
-index = np.argmax(predictions)
-
-print(class_names[index])
-
-type_4 = Image.open('img_4/10.png').convert('RGB')
-type_4 = type_4.resize((width, height))
-
-plt.imshow(type_4)
-plt.show()
-
-type_4_x = np.expand_dims(type_4, axis=0)
-predictions = model.predict(type_4_x)
-index = np.argmax(predictions)
-
-print(class_names[index])
-
-
-#Live Predictions using camera
 
 CAMERA = cv2.VideoCapture(1)
 camera_height = 500
 
-while True:
+while(True):
     _, frame = CAMERA.read()
-
+    
     # Flip
     frame = cv2.flip(frame, 1)
-
-    # Rescale the image output
+    
+    # Rescale the images output
     aspect = frame.shape[1] / float(frame.shape[0])
-    res = int(aspect * camera_height)  # Landscape orientation - wide image
+    res = int(aspect* camera_height)
     frame = cv2.resize(frame, (res, camera_height))
-
-    # Get ROI
-    roi = frame[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
-
-    # Parse BRG to RGB
-    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
-
+    
+    # Get roi
+    roi = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
     # Adjust alignment
     roi = cv2.resize(roi, (width, height))
-    roi = np.expand_dims(roi, axis=0)
+    roi_x = np.expand_dims(roi, axis=0)
 
-    predictions = model.predict(roi)
+    predictions = model.predict(roi_x)
     type_1_x, type_2_x, type_3_x, type_4_x = predictions[0]
+    
+    # The green rectangle
+    cv2.rectangle(frame, (300, 75), (650, 425), (0, 255, 0), 2)
+    
+    # Predictions / Labels
+    type_1_txt = '{}: {}%'.format(class_names[0], int(type_1_x*100))
+    cv2.putText(frame, type_1_txt, (70, 210), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
 
-    # Green rectangle
-    cv2.rectangle(frame, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (0, 255, 0), 2)
-
-
-    # Predictions/Labels
-    type_1_text = '{} - {}%'.format(class_names[0], int(type_1_x * 100))
-    cv2.putText(frame, type_1_text, (70, 210), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
-
-    type_2_text = '{} - {}%'.format(class_names[1], int(type_2_x * 100))
-    cv2.putText(frame, type_2_text, (70, 235), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
-
-    type_3_text = '{} - {}%'.format(class_names[2], int(type_3_x * 100))
-    cv2.putText(frame, type_3_text, (70, 255), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
-
-    type_4_text = '{} - {}%'.format(class_names[3], int(type_4_x * 100))
-    cv2.putText(frame, type_4_text, (70, 275), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
-
-    cv2.imshow('Real-time object detection', frame)
-
-    # Controls q = quit
+    type_2_txt = '{}: {}%'.format(class_names[1], int(type_2_x*100))
+    cv2.putText(frame, type_2_txt, (70, 235), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
+    
+    type_3_txt = '{}: {}%'.format(class_names[2], int(type_3_x*100))
+    cv2.putText(frame, type_3_txt, (70, 255), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
+    
+    type_4_txt = '{}: {}%'.format(class_names[3], int(type_4_x*100))
+    cv2.putText(frame, type_4_txt, (70, 275), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
+    
+    cv2.imshow('Real time object detection', frame)
+    
+    # Conrols q = quit / s = capturing
     key = cv2.waitKey(1)
-    if key & 0xFF == ord('q'):
+    
+    if key & 0xff == ord('q'):
         break
-
-# Release the camera
+    
+    # Preview
+    plt.imshow(frame)
+    plt.show()
+    
+# Camera
 CAMERA.release()
 cv2.destroyAllWindows()
+
